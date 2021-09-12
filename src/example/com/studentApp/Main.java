c90a81ecaea858e4c9c6237871e630c372361245
songdabin <82192898+songdabin@users.noreply.github.com>
2021-09-12 12:42:14 +0900

    second commit

--- src/example/com/studentApp/Main.java
+++ src/example/com/studentApp/Main.java
@@ -9,11 +9,16 @@ public class Main {
 	public static void main(String[] args) {
 		List<Student> al = new ArrayList<Student>();
 		System.out.println("Student List (ordered by name)");
-		Student s1 = new Student(101, "Kim", 23);
-		al.add(s1);
-		Student s2 = new Student(102, "Choi", 21);
-		al.add(s2);
+		al.add(new Student(101, "Kim", 23));
+		al.add(new Student(102, "Choi", 21));
 		al.add(new Student(103, "Park", 25));
+		al.add(new Student(104, "Song", 22));
+		al.add(new Student(105, "Han", 23));
+		al.add(new Student(106, "Lee", 27));
+		al.add(new Student(107, "Do", 24));
+		al.add(new Student(108, "Jung", 28));
+		al.add(new Student(109, "Jeon", 20));
+		al.add(new Student(110, "Shin", 29));
 		
 		Collections.sort(al);
 		
